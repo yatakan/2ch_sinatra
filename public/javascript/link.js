0389492e83df2efe55d0371909a0a8c1d2544fad
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-08-08 16:14:15 +0900

    gitignore?
    デプロイに向けてやってみます。

--- public/javascript/link.js
+++ public/javascript/link.js
@@ -1,13 +1,12 @@
 $(function() {
-  var number = ""
   var res = ""
   $(".text").map(function() {
+    // 000-999
     res = $(this).text().match(/>+\d{1,3}/)
     if (res) {
-      number = parseInt(res[0].match(/\d{1,3}/))
-      thread_id = parseInt($(this).parents('.thread').attr('id'))
-      new_text = $(this).text().replace(/>+\d{1,3}/,'<a href="' + '/threads/' + thread_id + '/' + number + '"' + '>>' + number + '</a>')
-        $(this).html(new_text)
+      var thread_id = parseInt($(this).parents('.thread').attr('id'))
+      var new_text = $(this).text().replace(/(>+)(\d{1,3})/g,'<a href="/threads/' + thread_id + '/$2">$1$2</a>')
     }
+    $(this).html(new_text);
   });
 });
