aca4b8deac41a9dff5f8db5d28dd50e801a44d59
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-08-10 20:25:02 +0900

    名前がある場合

--- 3ch.rb
+++ 3ch.rb
@@ -116,7 +116,7 @@ post '/post_thread' do
     if params[:name] == ""
       # client.query("INSERT INTO THREADS (title, mail, text, board_id, created_at, updated_at) values ('#{params[:title]}', '#{params[:mail]}', '#{params[:text]}', 1, now(), now() )")
     else
-      # client.query("INSERT INTO THREADS (title, name, mail, text, board_id, created_at, updated_at) values ('#{params[:title]}','#{params[:name]}', '#{params[:mail]}', '#{params[:text]}', 1, now(), now() )")
+      client.query("INSERT INTO THREADS (title, name, mail, text, board_id, created_at, updated_at) values ('#{params[:title]}','#{params[:name]}', '#{params[:mail]}', '#{params[:text]}', 1, now(), now() )")
     end
     # redirect "/board"
   end
